bd7b25c5175389e2b5c4c33df152b6858dd5dd0d
desguerra <ddesguerra@ucdavis.edu>
2022-07-19 17:23:40 -0700

    add numerical input for shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -149,29 +149,23 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
-    switch (shopOptionPrompt) {
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
-        case "REFILL":
-        case "refill":
+    switch (shopOptionPrompt) {
+        case 1:
             playerInfo.refillHealth();
             break;
-
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             break;
-        
         default:
             window.alert("You did not pick a valid option. Try again.");
-        
             shop();
             break;
       }
